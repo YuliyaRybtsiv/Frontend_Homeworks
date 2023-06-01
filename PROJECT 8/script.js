471d24a8557809f08a50ee51efcad693916e1450
Rybtsiv Yuliya <yuliyarybtsiv@gmail.com>
2023-06-01 20:39:46 +0200

    update

---
+++ PROJECT 8/script.js
@@ -0,0 +1,16 @@
+// alert("hello") // hello
+// console.log("hello")
+// const name = prompt("Как тебя зовут?")
+// console.log(name)
+
+// const userAge = 40
+// const userLogin = "Starta Institute"
+// console.log(userAge)
+// console.log("userAge")
+
+const productName = "Milk"
+const userName = "Arsen"
+const productPrice = 10000
+
+console.log(productName, productPrice)
+console.log(userName)
