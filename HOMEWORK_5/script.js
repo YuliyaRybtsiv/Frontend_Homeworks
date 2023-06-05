e2d44bdb6044901d4bbde59083b6d00249bc1281
Rybtsiv Yuliya <yuliyarybtsiv@gmail.com>
2023-06-05 18:51:10 +0200

    my new homework

---
+++ HOMEWORK_5/script.js
@@ -0,0 +1,29 @@
+// const number = +prompt("Введите число")
+// const sum = number * .10
+// console.log(sum)
+
+
+// const number1 = prompt("Введите число")
+// const number2 = prompt("Введите число")
+// const numberx = Math.min(number1, number2)
+// console.log(numberx)
+
+
+// const number = +prompt("Введите число")
+// if(number > 100) {
+//     console.log("Число больше 100")
+// } else if(number == 100) {
+//     console.log("Число равно 100")
+// } else {
+//     console.log("Число меньше 100")
+// } 
+
+
+// const name = pro mpt("Введите Ваше имя")
+// const yearofbirth = prompt("Введите Ваш год рождения")
+// const age = 2023 - yearofbirth
+// if(age >= 18) {
+//     console.log(`‘Hello, <${name}>’`)
+// } else {
+//     console.log(`‘Hi, <${name}>’`)
+// }
